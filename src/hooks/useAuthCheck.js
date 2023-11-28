b9f7d2d3fb8268481e09dd47ac03c78476612d50
kamruz-zzaman <kamruz.zaman8991@gmail.com>
2023-11-28 23:30:12 +0600

    redux toolkit setup done

---
+++ src/hooks/useAuthCheck.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useGetUserInfoQuery } from "../features/auth/authApi";
+import { getUserInfo } from "../features/auth/authSlice";
+
+const useAuthCheck = () => {
+  const [isAuth, setIsAuth] = useState(false);
+  const state = useSelector((state) => state.auth);
+  const { accessToken } = state || {};
+
+  const {
+    data: userData,
+    isLoading,
+    isError,
+  } = useGetUserInfoQuery(undefined, { skip: !accessToken });
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!accessToken || isError) {
+      setIsAuth(true);
+    }
+
+    if (!isLoading && !isError && userData?.user?._id) {
+      dispatch(getUserInfo(userData?.user));
+      setIsAuth(true);
+    }
+  }, [isAuth, setIsAuth, isError, isLoading, userData?.user?._id, getUserInfo]);
+
+  return isAuth;
+};
+
+export default useAuthCheck;
