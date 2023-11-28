0c2f6daa0372bdb2cb1e12c8be067c31fa857d72
kamruz-zzaman <kamruz.zaman8991@gmail.com>
2023-11-28 15:40:16 +0600

    header design create

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Header from "./components/shared-components/Header";
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/shared-components/Header.jsx
@@ -0,0 +1,55 @@
+const Header = () => {
+  return (
+    <div>
+      <nav className="bg-yellow-50 shadow shadow-yellow-100 w-100 px-8 md:px-auto">
+        <div className="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
+          <div className="md:order-1">
+            <img className="w-52" src="/src/assets/logo.png" alt="" />
+          </div>
+
+          <div className="order-2 md:order-3">
+            <button className="px-4 py-2 bg-orange-400 hover:bg-orange-500 text-gray-50 rounded-xl flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <span>Login</span>
+            </button>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
+
+{
+  /* <div className="text-gray-500 order-3 w-full md:w-auto md:order-2">
+  <ul className="flex font-semibold justify-between">
+    <li className="md:px-4 md:py-2 text-indigo-500">
+      <a href="#">Dashboard</a>
+    </li>
+    <li className="md:px-4 md:py-2 hover:text-indigo-400">
+      <a href="#">Search</a>
+    </li>
+    <li className="md:px-4 md:py-2 hover:text-indigo-400">
+      <a href="#">Explore</a>
+    </li>
+    <li className="md:px-4 md:py-2 hover:text-indigo-400">
+      <a href="#">About</a>
+    </li>
+    <li className="md:px-4 md:py-2 hover:text-indigo-400">
+      <a href="#">Contact</a>
+    </li>
+  </ul>
+</div>; */
+}
